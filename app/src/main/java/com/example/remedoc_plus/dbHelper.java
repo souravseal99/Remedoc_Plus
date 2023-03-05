a09bd5b2a85b419c237e353d3c1c71afb298ed45
Sourav Seal <sourav.seal1999@gmail.com>
2023-03-05 18:49:02 +0530

    `login setup`

---
+++ app/src/main/java/com/example/remedoc_plus/dbHelper.java
@@ -0,0 +1,35 @@
+package com.example.remedoc_plus;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class dbHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "loginInfo.db";
+    private static final int DB_VER = 1;
+
+    public dbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTableQuery = "CREATE TABLE users ("
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + "username TEXT,"
+                + "password TEXT,"
+                + "email TEXT"
+                + ")";
+        sqLiteDatabase.execSQL(createTableQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String dropTableQuery = "DROP TABLE IF EXISTS users";
+        sqLiteDatabase.execSQL(dropTableQuery);
+        onCreate(sqLiteDatabase);
+    }
+}
